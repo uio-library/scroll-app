679402c315d6ee7e97a436526031875851e6ef37
Henrik Sveinsson <henrik.sveinsson@me.com>
2017-08-25 11:47:34 +0200

    Webpage skeleton finised

--- resources/views/course.blade.php
+++ resources/views/course.blade.php
@@ -7,6 +7,7 @@
 
 	.moduleHeader {
 		font-size: 8vw;
+		color : white;
 	}
 
 
@@ -23,6 +24,18 @@
 	body {
 		padding-bottom: 6cm;
 	}     
+	.caption {
+	    position: absolute;
+	    top : 50%;
+	    left: 10%;
+	    width: 60%;
+	    height: 100%;
+	}
+
+	.thumbnail {
+		position: relative;
+	}
+
 </style> 
 
 <html>
@@ -32,7 +45,7 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 	<meta name="csrf-token" content="{{ csrf_token() }}">
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
-	<link rel="stylesheet" href="{{ elixir('css/app.css') }}">
+	<!--<link rel="stylesheet" href="{{ elixir('css/app.css') }}">-->
 	<title>Litteratur og bibliotek</title>
 
 </head>
@@ -42,65 +55,45 @@
 		<h1 class="mainHeader">LITTERATUR OG BIBLIOTEK<br></h1>
 		
 
-		<div class="module">
-			<div class="carousel" href="#biblioteket" data-toggle="collapse">
-				<div class="carousel-item active">
-					<img src="images/discoveredonceagain.jpg" class="img-fluid" >
-					<div class="carousel-caption left-align">
-						<h1 class=moduleHeader>1.0 Biblioteket</h1>
-					</div>
-				</div>
+		<div class = "module">
+			<div class="thumbnail text-left" data-toggle="collapse" data-target="#biblioteket">
+				<img src="images/discoveredonceagain.jpg" class="img-fluid" >
+				<div class="caption"><h1 class="moduleHeader">1.0 Biblioteket<h1></div>
 			</div>
-			<div id="biblioteket" class="collapse hide" role="tabpanel" aria-labelledby="headingOne">
+			<div id="biblioteket" class="collapse">
 				@include('contents.biblioteket')
 			</div>
 		</div>
 
-
-		<div class="module">
-			<div class="carousel" href="#litteratursok" data-toggle="collapse">
-				<div class="carousel-item active">
-					<img src="images/discoveredonceagain.jpg" class="img-fluid" >
-					<div class="carousel-caption">
-						<h1 class=moduleHeader>1.1 Litteratursøk</h1>
-					</div>
-				</div>
+		<div class = "module">
+			<div class="thumbnail text-left" data-toggle="collapse" data-target="#litteratursok">
+				<img src="images/discoveredonceagain.jpg" class="img-fluid" >
+				<div class="caption"><h1 class="moduleHeader">1.1 Litteratursøk<h1></div>
 			</div>
-			<div id="litteratursok" class="collapse hide" role="tabpanel" aria-labelledby="headingOne">
+			<div id="litteratursok" class="collapse">
 				@include('contents.litteratursok')
 			</div>
 		</div>
 
-
-		<div class="module">
-			<div class="carousel" href="#referering" data-toggle="collapse">
-				<div class="carousel-item active">
-					<img src="images/discoveredonceagain.jpg" class="img-fluid" >
-					<div class="carousel-caption">
-						<h1 class=moduleHeader>1.2 Referanse&shy;håndtering</h1>
-					</div>
-				</div>
+		<div class = "module">
+			<div class="thumbnail text-left" data-toggle="collapse" data-target="#referering">
+				<img src="images/discoveredonceagain.jpg" class="img-fluid" >
+				<div class="caption"><h1 class="moduleHeader">1.2 Referanse&shy;håndtering<h1></div>
 			</div>
-			<div id="referering" class="collapse hide" role="tabpanel" aria-labelledby="headingOne">
+			<div id="referering" class="collapse">
 				@include('contents.referering')
 			</div>
 		</div>
 
-
-		<div class="module">
-			<div class="carousel" href="#prove" data-toggle="collapse">
-				<div class="carousel-item active">
-					<img src="images/discoveredonceagain.jpg" class="img-fluid" >
-					<div class="carousel-caption">
-						<h1 class=moduleHeader>1.3 Prøve</h1>
-					</div>
-				</div>
+		<div class = "module">
+			<div class="thumbnail text-left" data-toggle="collapse" data-target="#prove">
+				<img src="images/discoveredonceagain.jpg" class="img-fluid" >
+				<div class="caption"><h1 class="moduleHeader">1.3 Prøve<h1></div>
 			</div>
-			<div id="prove" class="collapse hide" role="tabpanel" aria-labelledby="headingOne">
+			<div id="prove" class="collapse">
 				@include('contents.test')
 			</div>
 		</div>
-		
 
 	</div>
 	
