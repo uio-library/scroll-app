f6f2efbba12579eb1305bb14262016318a0e0f45
Dan Michael O. Heggø <danmichaelo@gmail.com>
2017-09-06 19:34:38 +0200

    Add some style to MultipleChoiceQuestion

--- resources/assets/js/components/MultipleChoiceQuestion.vue
+++ resources/assets/js/components/MultipleChoiceQuestion.vue
@@ -1,4 +1,31 @@
 <style lang="sass">
+@import '~bootstrap/scss/bootstrap.scss';
+
+.exercise
+    .alternatives
+        margin-top: 0.5rem
+        margin-bottom: 0.5rem
+
+    .alternative
+        margin: 0
+        label
+            display: block
+            border: 1px solid transparent
+            padding: 0.5rem 0.5rem 0.5rem 30px
+            height: 100%
+            border-radius: 3px
+
+            &:hover
+                border-color: #ececec
+
+            &.selected
+                border-color: map-get($theme-colors, "primary")
+
+        // By default, scale any image contained in the alternative to fill its width
+        img
+            display: block
+            width: 100%
+
 </style>
 
 <template lang="pug">
