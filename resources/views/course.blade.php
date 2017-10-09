16d91589c618fdf1a9f205c8781cdf9b03bd9c68
Henrik Andersen Sveinsson <henrik.sveinsson@me.com>
2017-10-09 13:14:54 +0200

    added header text option

--- resources/views/course.blade.php
+++ resources/views/course.blade.php
@@ -15,14 +15,18 @@
 
 <body>
 	<div id="app">
-		<div class="uio-banner" style="background-image:url(/images/kjemisk_logo.svg)">
+		<div class="uio-banner" style="background-image:url({{ $course->header }})">
 		</div> 
+		
+		<!-- <h2 class="header-text">
+			{{ $course->headertext }}
+		</h2> -->
 
 		@foreach ($course->modules as $id => $module)
 		<div class = "module">
 			<div class="header" data-toggle="collapse" data-target="#{{ $id }}" style="background-image:url({{ $module->image }})">
 				<div class="container-fluid">
-					<h2>{{ $module->name }}</h2>
+					<h2 style="user-select: none;">{{ $module->name }}</h2>
 				</div>
 			</div>
 			<div id="{{ $id }}" class="collapse container-fluid" style="margin-top : 10px">
@@ -38,6 +42,9 @@
 	<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
 	<script src="{{ elixir('js/app.js') }}"></script>
+	<script type="text/javascript" async
+	src="https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.2/MathJax.js?config=TeX-MML-AM_CHTML">
+</script>
 </body>
 
 </html>
