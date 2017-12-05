d6b00539eb0fd63e821be9214bcfa42e776f08e7
Dan Michael O. Heggø <danmichaelo@gmail.com>
2017-12-05 19:47:29 +0100

    Cleanup YoutubeVideo style, use sass

--- resources/assets/js/components/YoutubeVideo.vue
+++ resources/assets/js/components/YoutubeVideo.vue
@@ -1,98 +1,105 @@
-<style>
-.wrapper {
-    max-width: 960px;
-    margin: 0 auto;
-    padding: 0;
-}
+<style lang="sass">
 
-.youtube {
-    background-color: #000;
-    margin-bottom: 30px;
-    position: relative;
-    padding-top: 56.25%;
-    overflow: hidden;
-    cursor: pointer;
-}
-.youtube img {
-    width: 100%;
-    top: -16.82%;
-    left: 0;
-    opacity: 0.7;
-}
-.youtube .play-button {
-    width: 90px;
-    height: 60px;
-    background-color: #333;
-    box-shadow: 0 0 30px rgba( 0,0,0,0.6 );
-    z-index: 1;
-    opacity: 0.8;
-    border-radius: 6px;
-}
-.youtube .play-button:before {
-    content: "";
-    border-style: solid;
-    border-width: 15px 0 15px 26.0px;
-    border-color: transparent transparent transparent #fff;
-}
-.youtube img,
-.youtube .play-button {
-    cursor: pointer;
-}
-.youtube img,
-.youtube iframe,
-.youtube .play-button,
-.youtube .play-button:before {
-    position: absolute;
-}
-.youtube .play-button,
-.youtube .play-button:before {
-    top: 50%;
-    left: 50%;
-    transform: translate3d( -50%, -50%, 0 );
-}
-.youtube iframe {
-    height: 100%;
-    width: 100%;
-    top: 0;
-    left: 0;
-}
+.card.youtube
+    border: 0
+    margin: 1em 0
 
-.no-border {
-    border : none;
-    
-}
+    .card-body, .card-header
+        padding: 0
 
-.no-border .card-body {
-    padding : 0;
-}
+    .card-header button
+        cursor: pointer
+        text-align: left
+
+        &::before
+            display: inline-block
+            content: "-"
+            width: .7em
+
+    &.collapse .card-header button::before
+        content: "+"
+
+    .wrapper
+        max-width: 960px
+        margin: 0 auto
+        padding: 0
+
+    .player
+        background-color: #000
+        margin-bottom: 0
+        position: relative
+        padding-top: 56.25%
+        overflow: hidden
+        cursor: pointer
+
+        img
+            cursor: pointer
+            width: 100%
+            top: -16.82%
+            left: 0
+            opacity: 0.7
+
+        .play-button
+            cursor: pointer
+            width: 90px
+            height: 60px
+            background-color: #333
+            box-shadow: 0 0 30px rgba( 0,0,0,0.6 )
+            z-index: 1
+            opacity: 0.8
+            border-radius: 6px
+
+            &:before
+                content: ""
+                border-style: solid
+                border-width: 15px 0 15px 26.0px
+                border-color: transparent transparent transparent #fff
+
+
+        img, iframe, .play-button, .play-button:before
+            position: absolute
+
+        .play-button, .play-button:before
+            top: 50%
+            left: 50%
+            transform: translate3d( -50%, -50%, 0 )
+
+        iframe
+            height: 100%
+            width: 100%
+            top: 0
+            left: 0
 
 </style>
 
 <template>
-    <b-card class="no-border">
-    <div class=card-header @click="showCollapse = !showCollapse; !showCollapse && playing ? pauseVideo() : null" :class="showCollapse ? 'collapsed' : null">
-          <h5 class="mb-0"><div style="display:inline-block;width:1em">{{showCollapse ? "-":"+"}}</div> Video</h5>
-    </div>
+    <b-card class="youtube" :class="{collapse: !showCollapse}">
 
-    <b-collapse :id="uid" v-model="showCollapse" @show="onShow" @hide="onHide">
-      <div class="card-body">
-        <div>
-            <div class="wrapper">
-                <div class="youtube" @click="() => playing=true">
-                    <div class="play-button" v-show="!playing"></div>
-                    <img :src="thumb" alt="YouTube thumbnail">
-                    <iframe ref="youtubevideo" v-if="playing"
-                        :width="width"
-                        :height="height"
-                        :src="'https://www.youtube-nocookie.com/embed/' + id + '?rel=0&amp;showinfo=0&amp;autoplay=1&amp;enablejsapi=1'"
-                        frameborder="0"
-                        allowfullscreen
-                    ></iframe>
+        <b-card-header>
+            <b-button block variant="outline-secondary" @click="showCollapse = !showCollapse">
+                Video
+            </b-button>
+        </b-card-header>
+
+        <b-card-body>
+            <b-collapse :id="uid" v-model="showCollapse" @show="onShow" @hide="onHide">
+                <div class="wrapper">
+                    <div class="player" @click="() => playing=true">
+                        <div class="play-button" v-show="!playing"></div>
+                        <img :src="thumb" alt="YouTube thumbnail">
+                        <iframe v-if="playing"
+                            ref="iframe"
+                            :width="width"
+                            :height="height"
+                            :src="'https://www.player-nocookie.com/embed/' + id + '?rel=0&amp;showinfo=0&amp;autoplay=1&amp;enablejsapi=1'"
+                            frameborder="0"
+                            allowfullscreen
+                        ></iframe>
+                    </div>
                 </div>
-            </div>
-        </div>
-    </div>
-    </b-collapse>
+            </b-collapse>
+        </b-card-body>
+
     </b-card>
 </template>
 
@@ -132,13 +139,18 @@
         },
         methods: {
             pauseVideo : function() {
-                this.$refs.youtubevideo.contentWindow.postMessage('{"event":"command","func":"pauseVideo","args":[]}', '*');
+                this.$refs.iframe.contentWindow.postMessage(JSON.stringify({
+                    event: 'command',
+                    func: 'pauseVideo',
+                    args: [],
+                }), '*');
             },
             onShow: function() {
                 persistentState.put(this.uid, true);
             },
             onHide: function() {
                 persistentState.put(this.uid, false);
+                if (this.playing) this.pauseVideo();
             },
         }
     }
