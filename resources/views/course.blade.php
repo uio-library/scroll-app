7572457c382b744da2374cbec71ad09a05a50b27
Henrik Andersen Sveinsson <henrik.sveinsson@me.com>
2017-12-12 11:10:34 +0100

    using uio-footer and headertext

--- resources/views/course.blade.php
+++ resources/views/course.blade.php
@@ -10,20 +10,20 @@
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" integrity="sha256-eZrrJcwDc/3uDhsdt61sL2oOBY362qM3lon1gyExkL0=" crossorigin="anonymous" />
 
 	<link rel="stylesheet" href="{{ elixir('css/app.css') }}">
-	<title>Litteratur og bibliotek</title>
+	<title>Nettkurs fra UiO</title>
 
 </head>
 
 <body>
 	<div id="app">
-		<div class="uio-banner" style="background-image:url({{ $course->header }})">
-		</div>
+		<div class="uio-banner" style="background-image:url({{ $course->header }})">{!! $course->headertext !!}</div>
 
 		@foreach ($course->modules as $id => $module)
 		<course-module class="module" id="{{ $id }}" image="{{ $module->image }}" name="{{ $module->name }}">
 			{!! $module->html !!}
 		</course-module>
 		@endforeach
+		<div class="uio-footer" style="background-image:url({{ $course->footer }})"></div>
 	</div>
 
 	<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
