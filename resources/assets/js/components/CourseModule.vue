e300e6a0a8374f85560b24864dc87e472e8b9694
Dan Michael O. Heggø <danmichaelo@gmail.com>
2018-08-14 13:45:03 +0200

    Add accordion behaviour, #10, #11
    Using vue-scrollto to avoid scrolling catastrophes when opening a module.

--- resources/assets/js/components/CourseModule.vue
+++ resources/assets/js/components/CourseModule.vue
@@ -1,11 +1,11 @@
 <template>
     <div>
-        <a :href="'#' + moduleId" :id="moduleId" ref="h2button" class="header" v-on:click.prevent v-b-toggle="'collapse-' + moduleId"
+        <a :href="'#' + moduleId" :id="moduleId" ref="h2button" class="header" v-on:click.prevent="onClick" v-b-toggle="'collapse-' + moduleId"
             :style="{ height: headerHeight + 'px', backgroundImage: 'url(' + image +')' }">
                 <h2 style="user-select: none;">{{ name }}</h2>
         </a>
 
-        <b-collapse :id="'collapse-' + moduleId" v-model="showCollapse" @show="onShow" @hide="onHide">
+        <b-collapse :id="'collapse-' + moduleId" v-model="showCollapse" @show="onShow" @hide="onHide" accordion="my-acc">
             <b-container fluid style="padding-top : 1em; padding-bottom: 2em;">
                 <slot></slot>
             </b-container>
@@ -46,9 +46,29 @@
             updateHeaderHeight() {
                 this.headerHeight = Math.floor(this.$refs.h2button.offsetWidth / this.imageAspectRatio) - 1;
             },
+            onClick: function(ev) {
+                //
+            },
             onShow: function() {
                 persistentState.put(this.uid, true);
                 history.pushState(null, null, '#' + this.moduleId);
+
+                let el = this.$refs.h2button.parentElement.previousSibling;
+                while (el.nodeType !== Node.ELEMENT_NODE) {
+                    el = el.previousSibling;
+                }
+                if (!el.classList.contains('module')) {
+                    // First module
+                    return;
+                }
+                let cancelScroll = this.$scrollTo(
+                    el,
+                    300, // The collapse animation is 350 ms. Using a slightly shorter or longer duration seems to look best.
+                    {
+                        offset: el.getBoundingClientRect().height,
+                        easing: 'ease',
+                    }
+                );
             },
             onHide: function() {
                 persistentState.put(this.uid, false);
